4733cfaa61f0564fb11bcdaa88e907f920985b3b
mark.apgar@gmail.com <mark.apgar@gmail.com>
2024-06-26 02:16:53 +0000

    Edited NatureGuides.PersistNatureGuides

--- server_code/NatureGuides/PersistNatureGuides.py
+++ server_code/NatureGuides/PersistNatureGuides.py
@@ -7,11 +7,31 @@ class PersistNatureGuides():
   def __init__(self):
     pass
 
+  def row_to_dict(self, row):
+    if row is None:
+      return None
+      
+    d = dict(row)
+    d['_id'] = row.get_id()
+    return d
+
+  def search_to_dict(self, search_iterator):
+    results = []
+    for row in search_iterator:
+      results.append(self.row_to_dict(row))
+    return results  
+  
   def find_id_for_title(self, title):
     row = app_tables.guides.get(title=q.ilike(title))
     if row is not None:
       return row.get_id()
     return None
+
+  # def find_id_for_title(self, title):
+  #   row = app_tables.guides.get(title=q.ilike(title))
+  #   if row is not None:
+  #     return row.get_id()
+  #   return None
 
   def add_guide(self, title):
     return app_tables.guides.add_row(title=title).get_id()
